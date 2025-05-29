c46ac8bac3530d21496ff30748f016134fd7aa71
bo zhou <riiyzhou@gmail.com>
2025-05-29 10:51:38 +0800

    [ref] Refactor: Move completion-related functions to utils.py
    This commit refactors the code by moving the completion-related functions to a separate `utils.py` file. This improves code organization and reusability.
    
    The following changes were made:
    
    - Moved `read_json` and `query_complitions` to `utils.py`.
    - Updated `generate_bench_data.py` and `generate_mix_model_data.py` to import these functions from `utils.py`.

--- generate_mix_model_data.py
+++ generate_mix_model_data.py
@@ -1,70 +1,9 @@
-import asyncio, aiohttp
-import json, re
-import os, random
+import asyncio
+import os
 import argparse, sys
 
 from loguru import logger
-from requests import get
-timeout = aiohttp.ClientTimeout(total=600)  # 设置超时时间
-
-
-def read_json(input_file):
-    with open(input_file, encoding="utf-8") as f:
-        data_list = json.load(f)
-    return data_list
-
-
-def replace_tag_content(content, tag):
-    pattern = fr'(<{tag}>).*?(</{tag}>)'
-    replacement = r''
-    return re.sub(pattern, replacement, content, flags=re.DOTALL)
-
-
-def get_models(endpoint="http://127.0.0.1:11434", api_key="xxx"):
-    resp = get(
-        f"{endpoint}/v1/models",
-        headers={"Authorization": f"Bearer {api_key}"},
-        timeout=1000,
-    )
-    logger.info(resp.text)
-    return resp.json()
-    
-
-async def completion(cnt, item, predictions, endpoint="http://127.0.0.1:11434", api_key="xxx", model_name="", params: dict= {}):
-    prompt = f"{item['instruction']}\n{item['question']}"
-    messages = [{"role": "system", "content": "你是一个法官，旨在针对各种案件类型、审判程序和事实生成相应的法院裁决。你的回答不能含糊、有争议或者离题"},{"role": "user", "content": prompt}]
-    req_json = {"messages": messages, "repetition_penalty": 1.05, "temperature": 0.7, "top_k": 20, "top_p": 0.8}
-    if model_name:
-        req_json['model'] = model_name 
-    if params and isinstance(params, str):
-        req_json.update(json.loads(params))
-    logger.info(req_json)
-    async with aiohttp.ClientSession() as session:
-        async with session.post(f"{endpoint}/v1/chat/completions", json=req_json, headers={"Authorization": f"Bearer {api_key}"},timeout=timeout) as response:
-            resp = await response.json()
-            prediction = resp['choices'][0]['message']["content"] or resp['choices'][0]['message']["reasoning_content"] or ""
-            prediction = replace_tag_content(prediction, 'think').replace("<></>", "").strip()
-            predictions[f"{cnt}"] = {
-                    "origin_prompt": prompt.replace('句子：\n\n', '句子：\n'),
-                    "prediction": prediction,
-                    "refr": item["answer"],
-                }
-            logger.info(prediction)
-
-
-async def new_func(endpoint, api_key, model_name, params, output_file, data_list):
-    predictions = {}
-    tasks = []
-    for cnt, item in enumerate(random.sample(data_list, 100)):
-        try:
-            task = asyncio.create_task(completion(cnt, item, predictions, endpoint=endpoint, api_key=api_key, model_name=model_name, params=params))
-        except Exception as E:
-            logger.info(E)
-            continue
-        tasks.append(task)
-    await asyncio.gather(*tasks, return_exceptions=False)
-    with open(output_file, "w") as f:
-        f.write(json.dumps(predictions, ensure_ascii=False))
+from utils import read_json, query_complitions
 
 
 def main(argv):
@@ -114,7 +53,7 @@ def main(argv):
             endpoint = f"{base_url}:8000"
         logger.info(endpoint)
         logger.info(input_file)
-        asyncio.run(new_func(endpoint, api_key, model_name, params, output_file, data_list))
+        asyncio.run(query_complitions(endpoint, api_key, model_name, params, output_file, data_list))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
