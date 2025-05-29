a45d048a0829a6a2fd2e92e84c4997688b2df12e
bo zhou <riiyzhou@gmail.com>
2025-05-29 13:25:06 +0800

    [feat] Add utility functions for LLM interaction
    - Implemented `read_json` to load JSON data from files.
    - Implemented `replace_tag_content` to remove specific tags and their content from a string using regular expressions.
    - Implemented `_exec_single_query` to send a single query to an LLM endpoint and process the response, including error handling and response parsing.
    - Implemented `query_complitions` to concurrently query an LLM endpoint with a list of prompts and save the results to a JSON file. It uses `asyncio` for concurrent execution.

---
+++ utils.py
@@ -0,0 +1,55 @@
+import asyncio, aiohttp
+import json, re
+
+from loguru import logger
+timeout = aiohttp.ClientTimeout(total=600)  # 设置超时时间
+
+
+def read_json(input_file):
+    with open(input_file, encoding="utf-8") as f:
+        data_list = json.load(f)
+    return data_list
+
+
+def replace_tag_content(content, tag):
+    pattern = fr'(<{tag}>).*?(</{tag}>)'
+    replacement = r''
+    return re.sub(pattern, replacement, content, flags=re.DOTALL)
+
+
+async def _exec_single_query(cnt, item, predictions, endpoint="http://127.0.0.1:11434", api_key="xxx", model_name="", params: dict= {}):
+    prompt = f"{item['instruction']}\n{item['question']}"
+    messages = [{"role": "system", "content": "你是一个法官，旨在针对各种案件类型、审判程序和事实生成相应的法院裁决。你的回答不能含糊、有争议或者离题"},{"role": "user", "content": prompt}]
+    req_json = {"messages": messages, "repetition_penalty": 1.05, "temperature": 0.7, "top_k": 20, "top_p": 0.8}
+    if model_name:
+        req_json['model'] = model_name 
+    if params and isinstance(params, str):
+        req_json.update(json.loads(params))
+    logger.info(req_json)
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{endpoint}/v1/chat/completions", json=req_json, headers={"Authorization": f"Bearer {api_key}"},timeout=timeout) as response:
+            resp = await response.json()
+            prediction = resp['choices'][0]['message']["content"] or resp['choices'][0]['message']["reasoning_content"] or ""
+            prediction = replace_tag_content(prediction, 'think').replace("<></>", "").strip()
+            predictions[f"{cnt}"] = {
+                    "origin_prompt": prompt.replace('句子：\n\n', '句子：\n'),
+                    "prediction": prediction,
+                    "refr": item["answer"],
+                }
+            logger.info(prediction)
+
+
+async def query_complitions(endpoint, api_key, model_name, params, output_file, data_list):
+    predictions = {}
+    tasks = []
+    for cnt, item in enumerate(data_list[:100]):
+        try:
+            task = asyncio.create_task(_exec_single_query(cnt, item, predictions, endpoint=endpoint, api_key=api_key, model_name=model_name, params=params))
+        except Exception as E:
+            logger.info(E)
+            continue
+        tasks.append(task)
+    await asyncio.gather(*tasks, return_exceptions=False)
+    with open(output_file, "w") as f:
+        f.write(json.dumps(predictions, ensure_ascii=False))
+
